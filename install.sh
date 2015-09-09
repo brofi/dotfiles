8ccfcb63a03af12aa5863bbe04ae1f955a95a04f
Andy B <andyroid.b@gmail.com>
2015-09-09 03:21:33 +0200

    fix use of 'find' for calls from other directories

--- install.sh
+++ install.sh
@@ -9,18 +9,17 @@ this_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo 'Ensuring directory structure...'
 # Dirs without children (only links to parent and itself)
 # Ingore .git directory
-directories=$(find -type d -links 2 -not -path './.git/*')
+directories=$(find $this_dir -type d -links 2 -not -path "$this_dir/.git/*")
 for d in $directories; do
-    mkdir -p $HOME/${d#*/}
+    mkdir -p $HOME/${d#$this_dir/}
 done
 
 echo 'Setting up symlinks...'
 # Get file names, ignore this file and README.md
 # Ingore files in .git directory
-dotfiles=$(find -type f \( ! -name ${0#*/} ! -name README.md \) -not -path './.git/*')
+dotfiles=$(find $this_dir -type f \( ! -name ${0#*/} ! -name README.md \) -not -path "$this_dir/.git/*")
 for f in $dotfiles; do
-    target=$this_dir/${f#*/}
-    name=$HOME/${f#*/}
+    name=$HOME/${f#$this_dir/}
     # Save existing files
     if [ -f $name ]; then
         bak=${name}.dotsave
@@ -30,5 +29,5 @@ for f in $dotfiles; do
         mv $name $bak
         echo "warning: $name saved as $bak"
     fi
-    ln -sv $target $name
+    ln -sv $f $name
 done
