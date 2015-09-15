b3cc5c7c2a9c7437bf1b748fbf81039f37e5f017
Andy B <andyroid.b@gmail.com>
2015-09-16 01:46:44 +0200

    install dir cmd line arg

--- install.sh
+++ install.sh
@@ -5,6 +5,23 @@ __self="$(basename "${BASH_SOURCE[0]}")"
 # Full path of this file's directory
 __dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+# By default links are installed in user's home directory
+install_dir=$HOME
+
+# Parse key value pairs
+while [[ $# > 1 ]]; do
+    key="$1"
+    case $key in
+        -C)
+            install_dir="$2"
+            shift
+            ;;
+        *)
+            # unknown
+    esac
+    shift
+done
+
 IFS=$'\n'
 # Get dirs without children (only links to parent and itself)
 # Prune content of .git directory and exclude .git itself
@@ -17,12 +34,12 @@ unset IFS
 
 echo 'Ensuring directory structure...'
 for d in "${leaf_dirs[@]}"; do
-    mkdir -p $HOME/${d#$__dir/}
+    mkdir -p $install_dir/${d#$__dir/}
 done
 
 echo 'Setting up symlinks...'
 for f in "${dotfiles[@]}"; do
-    name=$HOME/${f#$__dir/}
+    name=$install_dir/${f#$__dir/}
     # Save existing files
     if [ -f "$name" ]; then
         bak=${name}.dotsave
